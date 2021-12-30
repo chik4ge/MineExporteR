aa8609c39f2cfcf7c191ddc0277fa99edf9d217d
chikage <manadora1205@gmail.com>
2021-12-31 05:06:18 +0900

    equals,hashCode実装

--- src/main/java/com/chikage/mineexporter/utils/UV.java
+++ src/main/java/com/chikage/mineexporter/utils/UV.java
@@ -1,5 +1,7 @@
 package com.chikage.mineexporter.utils;
 
+import java.util.Objects;
+
 public class UV {
     public final float u;
     public final float v;
@@ -7,5 +9,18 @@ public class UV {
     public UV(float u, float v) {
         this.u = u;
         this.v = v;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UV uv = (UV) o;
+        return Float.compare(uv.u, u) == 0 && Float.compare(uv.v, v) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(u, v);
     }
 }
--- src/main/java/com/chikage/mineexporter/utils/Vertex.java
+++ src/main/java/com/chikage/mineexporter/utils/Vertex.java
@@ -1,5 +1,7 @@
 package com.chikage.mineexporter.utils;
 
+import java.util.Objects;
+
 public class Vertex {
     public final float x;
     public final float y;
@@ -9,5 +11,18 @@ public class Vertex {
         this.x = x;
         this.y = y;
         this.z = z;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vertex vertex = (Vertex) o;
+        return Float.compare(vertex.x, x) == 0 && Float.compare(vertex.y, y) == 0 && Float.compare(vertex.z, z) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y, z);
     }
 }
